7aba26a119820b21c70823e20ae15f9ca5116442
USER <y.hoshino25@gmail.com>
2022-07-01 00:05:21 +0900

    test spider framework KIMURAI

---
+++ README.md
@@ -0,0 +1,14 @@
+## Kimurai (scraping framework for Ruby)  
+Ruby版Scrapy(python)を試すリポ
+### Reference:
+https://github.com/vifreefly/kimuraframework
+https://qiita.com/hibiheion/items/502b1a4091d54d05fb53
+
+<br/>
+
+### 他、エラー対処等：
+chromedriverのバージョンエラーが出た際：
+https://qiita.com/iHacat/items/9c5c186f0d146bc98784
+
+「"chromedriver"は開発元を検証できないため開けません。」(PC側から)
+https://qiita.com/apukasukabian/items/77832dd42e85ab7aa568
---
+++ test02_spider.rb
@@ -0,0 +1,53 @@
+require "kimurai"
+
+class TestSpider < Kimurai::Base
+    @name = "kimurai_spider"
+    # スクレイピングに使用するドライバ
+    @engine = :selenium_chrome
+    # 最初に訪れるURL。配列で複数設定することも可能。
+    @start_urls = ["https://cyclemarket.jp"]
+    @config = {
+        # ユーザーエージェント
+        user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
+        before_request: { delay: 2 }
+    }
+
+    def parse(response, url:, data: {})
+        # CSSセレクタを使ってヘッダのナビゲーションを取得
+        # 項目の取得には「css」や「xpath」といったNokogiriの検索系メソッドが使用できる
+        response.css("#nav-global ul li a").each do |menu|
+            # skip if :
+            next if menu[:class].include?("outlet") || menu[:class].include?("parts")
+            # navから各urlを取得
+            category_name = menu.css("span.caption").text
+            category_url = absolute_url(menu[:href], base: url)
+    
+            request_to :parse_category_page, url: category_url, data: { category_name: category_name }
+        end
+    end
+
+    def parse_category_page(response, url:, data: {})
+        # 商品情報を取得する
+        response.css("#cy-products .cy-product-list .product a").each do |product|
+            # CSV出力のために1件ごとにハッシュに入れる
+            row = {}
+            row[:name] = product_name(product.css(".body .title").text.strip)
+            row[:category_name] = data[:category_name]
+            row[:min_price] = product.css(".min-price").text.strip.delete("^0-9")
+            row[:max_price] = product.css(".max-price").text.strip.delete("^0-9")
+    
+            # CSVファイルに出力する
+            save_to "results.csv", row, format: :csv
+        end
+    end
+
+    private
+
+    # 商品名にメーカー名がついている場合はメーカー名を取り除く
+    def product_name(base_name)
+        base_name.include?("\n") ? base_name.split("\n")[1].strip : base_name
+    end
+
+end
+
+TestSpider.crawl!
