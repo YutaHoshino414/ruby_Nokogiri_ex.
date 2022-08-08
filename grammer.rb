d15a5dba1c9d4473e32af105d85d27443ee9bcea
USER <y.hoshino25@gmail.com>
2022-08-09 00:02:44 +0900

    Add gitignore

---
+++ grammer.rb
@@ -0,0 +1,5 @@
+fruits = {"apple": "100円", "orange": "80円", "melon": "500"}
+
+p fruits
+# {:apple=>"100円", :orange=>"80円", :melon=>"500"}
+p fruits.class
--- test.rb
+++ test.rb
@@ -5,18 +5,25 @@ url = "https://www.keiostore.co.jp/business/store_list.html"
 res = URI.open(url)
 doc = Nokogiri::HTML.parse(res)
 
+# 属性値の取得
+#https://rooter.jp/web-crawling/nokogiri-method-attr/
+
 doc.xpath('//table[@class="storeList"]//tr/td[contains(@class,"storeName")]/a').each.with_index do |li, i|
     
-    url = "https://www.keiostore.co.jp/business/#{li.attribute('href')}"
+    p li.class  #Nokogiri::XML::Element
+    # how to get attribute? 
+    #li.attr('href')
+    #li.get_attribute('href')
+    #li['href']
+    href = li.attribute('href')
+    url = "https://www.keiostore.co.jp/business/#{href}"
+    p url
+
     name = li.text
     address = li.parent.parent.at_css('.address').text
     tel = li.parent.parent.at_css('.telNum').text
     time = li.parent.parent.at_css('.time').text
     parking = li.parent.parent.at_css('.parking').text
 
-    puts "#{li.parent.parent}"
-    #pp "#{url}\n#{name}\n#{address}"
-    puts "#{i}:\n#{url}\n#{name}\n#{address}\n#{tel}\n#{time}"
-    puts '----'
     
 end
